bf69cd00e19cdb7821cf84a83a50d67ecdd53e59
Santiago Cifuentes <santiagocifuentes66@hotmail.com>
2025-03-12 17:01:08 -0300

    Runner ahora corre los tests

--- run_solver.sh
+++ run_solver.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-python3 run_challenge.py . datasets/a output/
+directory="datasets/a"
+output="output/"
+
+python3 run_challenge.py . $directory $output
+
+for file in "$directory"/*; do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        echo "Result for $filename"
+        python3 "checker.py" $directory"/"$filename $output$filename
+    fi
+done
+
