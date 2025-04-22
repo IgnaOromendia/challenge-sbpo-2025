c84766b5baf0b0bab1a254f026a9e75dec48e482
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2025-04-22 15:18:13 -0300

    Parametriza procesador

--- process_results.py
+++ process_results.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import re
+import sys
 
 def amount_of_orders_instance():
     folder_path = "./datasets/a"
@@ -19,21 +20,28 @@ def lambda_gap(obj, real_obj):
     error = abs(obj - real_obj)
     return 0 if error < 1e-4 else error
 
-result_path = "./results/results_parametric_default.csv"
+def process_file(result_path):
+    instancias = amount_of_orders_instance()
 
-instancias = amount_of_orders_instance()
+    df = pd.read_csv(result_path)
+    df_gt = pd.read_csv("./results/ground_truth_set_a.csv")
 
-df = pd.read_csv(result_path)
-df_gt = pd.read_csv("./results/ground_truth_set_a.csv")
+    df.insert(0, "instancia", df["ordenes"].apply(lambda x: instancias.get(int(x), "Total")))
 
-df.insert(0, "instancia", df["ordenes"].apply(lambda x: instancias.get(int(x), "Total")))
-
-df = df.sort_values(by="instancia", ascending=True, ignore_index=True)
+    df = df.sort_values(by="instancia", ascending=True, ignore_index=True)
 
 
-df.insert(3, "error", df.apply(lambda row: lambda_gap(row["obj"], df_gt.loc[row.name, "obj"]), axis=1))
+    df.insert(3, "error", df.apply(lambda row: lambda_gap(row["obj"], df_gt.loc[row.name, "obj"]), axis=1))
 
-new_row = {"instancia": "Tiempo total", "tiempo": df["tiempo"].sum() / 60}
-df.loc[len(df)] = new_row
+    new_row = {"instancia": "Tiempo total", "tiempo": df["tiempo"].sum() / 60}
+    df.loc[len(df)] = new_row
 
-df.to_csv(result_path, index=False)
+    df.to_csv(result_path, index=False)
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2): 
+        print("Falta escribir el path del archivo a preocesar")
+        exit()
+    
+    process_file(sys.argv[1])
+    
