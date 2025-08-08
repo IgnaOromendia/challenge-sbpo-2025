932454b6241c716ec5c65d2d42cc482160e40037
Santiago Cifuentes <santiagocifuentes66@hotmail.com>
2025-08-08 20:11:28 -0300

    Cortes para singletons

--- src/main/java/org/sbpo2025/challenge/MIPSolver.java
+++ src/main/java/org/sbpo2025/challenge/MIPSolver.java
@@ -44,16 +44,9 @@ public abstract class MIPSolver extends CPLEXSolver {
         Y = new IloIntVar[this.aisles.size()];
 
         this.orderItemSum = new int[orders.size()];
-        List<Integer> orderSum = new ArrayList<>();
         
-        for(int o = 0; o < this.orders.size(); o++) {
+        for(int o = 0; o < this.orders.size(); o++)
             this.orderItemSum[o] = this.orders.get(o).values().stream().mapToInt(Integer::intValue).sum();
-            orderSum.add(orderItemSum[o]);
-        }
-
-        double percentil = 0.95;
-        orderSum.sort(null);
-        this.quantile = orderSum.get((int)Math.round(orderSum.size()*percentil));
     }
 
 
@@ -145,6 +138,9 @@ public abstract class MIPSolver extends CPLEXSolver {
 
             // Agrega restricciones de corte
             setConstraint(this.currentBest);
+
+            // Add cuts for singletons
+            addSingletonConstraints();
 
             if (extraCode != null) extraCode.run(this.cplex, this.X, this.Y);            
             
@@ -326,34 +322,6 @@ public abstract class MIPSolver extends CPLEXSolver {
 
             System.out.println("Before: num aisles is " + used_aisles.size());
 
-            Set<Integer> orders_before_small = new HashSet<>();
-            Set<Integer> orders_before_small_copy = new HashSet<>();
-            Set<Integer> orders_before_big = new HashSet<>();
-            Set<Integer> orders_before_big_copy = new HashSet<>();
-            Set<Integer> orders_before_quant = new HashSet<>();
-            Set<Integer> orders_before_quant_copy = new HashSet<>();
-
-            int threshold = 10;
-            for (int o : used_orders) {
-                if (this.orderItemSum[o] >= quantile) {
-                    orders_before_quant.add(o);
-                    orders_before_quant_copy.add(o);
-                }
-
-                if (this.orderItemSum[o] >= threshold) {
-                    orders_before_big.add(o);
-                    orders_before_big_copy.add(o);
-                } else {
-                    orders_before_small.add(o);
-                    orders_before_small_copy.add(o);
-                }
-            }
-
-            System.out.println("Before: num small orders is " + orders_before_small.size());
-            System.out.println("Before: num big orders is " + orders_before_big.size());
-            System.out.println("Before: total orders is " 
-                        + (orders_before_small.size() + orders_before_big.size()));
-
             used_orders.clear();
             used_aisles.clear();      
             
@@ -361,55 +329,13 @@ public abstract class MIPSolver extends CPLEXSolver {
             fillSolutionList(this.Y, used_aisles, this.aisles.size());
          
             Set<Integer> aisles_after = new HashSet<>(used_aisles);
-            Set<Integer> orders_after_small = new HashSet<>();
-            Set<Integer> orders_after_big = new HashSet<>();
-            Set<Integer> orders_after_quant = new HashSet<>();
-
-            for (int o : used_orders) {
-                if (this.orderItemSum[o] >= quantile)
-                    orders_after_quant.add(o);
-
-                if (this.orderItemSum[o] >= threshold) {
-                    orders_after_big.add(o);
-                } else {
-                    orders_after_small.add(o);
-                }
-            }
 
             aisles_before.removeAll(aisles_after);
             aisles_after.removeAll(aisles_before_copy);
 
             System.out.println("After: size is " + used_aisles.size());
             System.out.println("After: Difference between iterations is " 
-                    + (aisles_before.size() + aisles_after.size()));  
-            
-            System.out.println("After: num small orders is " + orders_after_small.size());
-            System.out.println("After: num big orders is " + orders_after_big.size());
-            System.out.println("After: num total orders is " 
-                            + (orders_after_small.size() + orders_after_big.size()));
-
-            orders_before_small.removeAll(orders_after_small);
-            orders_after_small.removeAll(orders_before_small_copy);
-        
-            System.out.println("After: Difference small orders "
-                                + (orders_before_small.size() + orders_after_small.size()));
-            
-            orders_before_big.removeAll(orders_after_big);
-            orders_after_big.removeAll(orders_before_big_copy);
-
-            System.out.println("After: Difference big orders "
-                                + (orders_before_big.size() + orders_after_big.size()));
-
-            System.out.println("After: Difference total orders "
-                                + (orders_before_big.size() + orders_after_big.size()
-                                + orders_before_small.size() + orders_after_small.size()));
-
-            orders_before_quant.removeAll(orders_after_quant);
-            orders_after_quant.removeAll(orders_before_quant_copy);
-
-            System.out.println("After: Difference quant orders "
-                                + (orders_before_quant.size() + orders_after_quant.size())
-                                + " (quantile is " + this.quantile + ")");
+                    + (aisles_before.size() + aisles_after.size()));
         }
     }
 
@@ -461,5 +387,49 @@ public abstract class MIPSolver extends CPLEXSolver {
         }
 
         return this.cplex.addGe(expr, used_aisles.size() - neighbourhoodSize);
+    }
+
+    void addSingletonConstraints() throws IloException {
+        int[] elementsCountInOrders = new int[this.nItems];
+        for (int e=0; e<this.nItems; e++) elementsCountInOrders[e] = 0;
+
+        List<List<Integer>> ordersPerElem = new ArrayList<>();
+        for (int e=0; e<this.nItems; e++) ordersPerElem.add(new ArrayList<>());
+
+        int numSizeOne = 0;
+        for (int o=0; o<this.orders.size(); o++) {
+            for (Map.Entry<Integer, Integer> entry : this.orders.get(o).entrySet()) {
+                int key = entry.getKey();
+                int value = entry.getValue();
+
+                if (this.orders.get(o).size() == 1 && value == 1) {
+                    numSizeOne++;
+                }
+
+                elementsCountInOrders[key] += value;
+                if (elementsCountInOrders[key] == 1) ordersPerElem.get(key).add(o);
+            }
+        }
+        System.out.println("Number of size one orders: " + numSizeOne);
+
+        int numSingletons = 0;
+
+        // Esto se puede hacer en lineal. Mejorar.
+        for (int e=0; e<this.nItems; e++) {
+            if (elementsCountInOrders[e] != 1) continue;
+            
+            int order = ordersPerElem.get(e).get(0);
+            if (this.orders.get(order).size() != 1) continue;
+
+            numSingletons++;
+            
+            for (int a=0; a<this.aisles.size(); a++) {
+                Map<Integer, Integer> aisle = this.aisles.get(a);
+                if (aisle.containsKey(e))
+                    this.cplex.addLe(Y[a], X[order]);
+            }
+        }
+
+        System.out.println("Number of singletons computed: " + numSingletons);
     }
 }
